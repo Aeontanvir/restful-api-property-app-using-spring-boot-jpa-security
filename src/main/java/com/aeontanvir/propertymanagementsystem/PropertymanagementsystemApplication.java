af6010c929d06a70b6da6c48137ac99810703829
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-15 23:18:17 -0500

    Swagger definition added

--- src/main/java/com/aeontanvir/propertymanagementsystem/PropertymanagementsystemApplication.java
+++ src/main/java/com/aeontanvir/propertymanagementsystem/PropertymanagementsystemApplication.java
@@ -1,11 +1,24 @@
 package com.aeontanvir.propertymanagementsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Property Management System Endpoints",
+				version = "1.0.0",
+				contact = @Contact(
+						name = "Md Tanvir Rahman Khan",
+						email = "aeontanvir@gmail.com"
+				)
+		)
+)
 public class PropertymanagementsystemApplication {
 
 	public static void main(String[] args) {
