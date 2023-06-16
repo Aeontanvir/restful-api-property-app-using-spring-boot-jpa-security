757539c1898357d3ec9758ab3c69fbe1d4fae3dd
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-15 20:42:55 -0500

    H2 config

--- src/main/java/com/aeontanvir/propertymanagementsystem/config/SecurityConfiguration.java
+++ src/main/java/com/aeontanvir/propertymanagementsystem/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +26,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/auth/**")
                 .permitAll()
-                .requestMatchers("/api/v2/others")
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -35,6 +36,7 @@ public class SecurityConfiguration {
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.headers().frameOptions().disable();
         return httpSecurity.build();
     }
 }
