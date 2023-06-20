0f4b14c2a1d9334cc3b75885939252acd0976205
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-20 11:37:34 -0500

    cors added

--- src/main/java/com/aeontanvir/propertymanagementsystem/config/CorsConfig.java
+++ src/main/java/com/aeontanvir/propertymanagementsystem/config/CorsConfig.java
@@ -1,15 +1,19 @@
 package com.aeontanvir.propertymanagementsystem.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .maxAge(3600);
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
